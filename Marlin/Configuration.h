fbc807c3323ba054316b5890104bede19d6d8abd
Sthopeless <6695214+Sthopeless@users.noreply.github.com>
2019-10-29 22:11:33 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1296,8 +1296,7 @@
  * Commands to execute at the end of G29 probing.
  * Useful to retract or move the Z probe out of the way.
  */
-//#define Z_PROBE_END_SCRIPT "G1 Z10 F12000\nG1 X15 Y330\nG1 Z0.5\nG1 Z10"
-#define Z_PROBE_END_SCRIPT "G1 Z10 F12000\nG1 X117.5 Y117.5"
+#define Z_PROBE_END_SCRIPT "G1 Z10 F12000\nG1 X30 Y30"
 
 // @section homing
 
